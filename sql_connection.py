0e67f0c97f986827a32d5dc63806afc46f14c026
Siddhant Mavani <siddhantmavani1@gmail.com>
2022-07-24 23:22:27 +0530

    Scan QR Code and generate bill

---
+++ sql_connection.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+# import mysql.connector
+__cnx = None
+
+
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        __cnx = sqlite3.connect('ssip.db', check_same_thread=False)   # username- siddhant password-sid
+
+    # __cnx = mysql.connector.connect(user='root', password='Siddhant@001',
+    #                                 host='127.0.0.1',
+    #                              database='ssip')'''
+
+    return __cnx
